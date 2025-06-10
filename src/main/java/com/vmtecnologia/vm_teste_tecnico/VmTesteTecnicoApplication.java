28179d36c764b99acc191a1a0499396534866662
vinicius-malmann <viniciusmalmann@gmail.com>
2025-06-10 10:24:57 -0300

    Primeiro commit

---
+++ src/main/java/com/vmtecnologia/vm_teste_tecnico/VmTesteTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.vmtecnologia.vm_teste_tecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmTesteTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmTesteTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmtecnologia/vm_teste_tecnico/VmTesteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmtecnologia.vm_teste_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmTesteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
